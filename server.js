917f6e172584cc3e3ab33f5d16044dfbf9b06d7c
Harshal Bapat <harshal.bapat@jacobs.com>
2025-05-19 17:41:45 +0530

    code changes made to server.js reverted

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors'); // Import cors
 const app = express();
 
 // Import your backend app
 const api = require('./server/index');
-
-// Use cors middleware
-app.use(cors()); // Correctly using cors as a middleware function
 
 // Serve API routes under /api
 app.use('/api', api);
